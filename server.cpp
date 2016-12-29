ecc57d38c1ed5e35fd524a35069793e3c60260a3
Naama Stempler <stempnaama@gmail.com>
2016-12-29 18:18:42 +0200

    try to push back to eti

--- server.cpp
+++ server.cpp
@@ -26,6 +26,7 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
+//hello eti
     /*testing::InitGoogleTest(&argc, argv);
     RUN_ALL_TESTS();*/
     cout << "Hello, from server" << endl;
